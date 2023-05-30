ab9bdfe80741ebe971ca14188e6987457afd47ef
Higor Jugler <higorjugler@hotmail.com>
2023-05-30 17:50:16 -0300

    Implementado NavHostFragment para a selecao do menu

--- app/src/main/java/daniel/lop/io/marvelappstarter/MainActivity.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/MainActivity.kt
@@ -2,15 +2,36 @@ package daniel.lop.io.marvelappstarter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import daniel.lop.io.marvelappstarter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * NavHostFragment is responsible for providing a hosting environment for Android Jetpack
+     * navigation, where target fragments are dynamically swapped to allow for a smooth and
+     * controlled navigation experience.
+     */
+    private lateinit var navHostFragment: NavHostFragment
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViews(binding)
+    }
+
+    private fun initViews(binding: ActivityMainBinding) {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener {  }
+        }
     }
 }
