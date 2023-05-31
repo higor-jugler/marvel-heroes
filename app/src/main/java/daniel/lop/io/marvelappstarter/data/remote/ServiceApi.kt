e5def4aa174042ccb68b207a52db3e13676ea3e5
Higor Jugler <higorjugler@hotmail.com>
2023-05-31 13:23:26 -0300

    Config Hilt no projeto e construido interface | ServiceApi

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/data/remote/ServiceApi.kt
@@ -0,0 +1,28 @@
+package daniel.lop.io.marvelappstarter.data.remote
+
+import daniel.lop.io.marvelappstarter.data.model.character.CharacterModelResponse
+import daniel.lop.io.marvelappstarter.data.model.comic.ComicModelResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+// Endpoint construction
+interface ServiceApi {
+
+    @GET("characters")
+    suspend fun list(
+        // Return characters with names that begin with the specified string (e.g. Sp).
+        @Query("nameStartsWith") nameStartsWith: String? = null
+    ): Response<CharacterModelResponse>
+
+    @GET("characters/{characterId}/comics")
+    suspend fun getComics(
+        // Fetches lists of comics containing a specific character, with optional filters.
+        @Path(
+            value = "characterId",
+            encoded = true
+        ) characterId: Int
+    ): Response<ComicModelResponse>
+}
---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,8 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+
+    const val BASE_URL = "https://gateway.marvel.com/V1/public/"
+    const val PUBLIC_KEY = "0f441f6a6b0411a343365c35ee0b8159"
+    const val PRIVATE_KEY = "49fa44f5216088bf02f693483c491525c9697cd0"
+}
