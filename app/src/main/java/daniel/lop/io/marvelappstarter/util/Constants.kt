59f8151501a74ab9ffd1a73d2eb63eec3bc49f2c
Higor Jugler <higorjugler@hotmail.com>
2023-06-06 17:59:34 -0300

    Montando uma Url com o OkHttpClient

--- app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -5,4 +5,8 @@ object Constants {
     const val BASE_URL = "https://gateway.marvel.com/V1/public/"
     const val PUBLIC_KEY = "0f441f6a6b0411a343365c35ee0b8159"
     const val PRIVATE_KEY = "49fa44f5216088bf02f693483c491525c9697cd0"
+
+    const val APIKEY = "apikey"
+    const val HASH = "hash"
+    const val TS = "ts"
 }
